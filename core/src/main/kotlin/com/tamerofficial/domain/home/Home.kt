ca17533f6e20f09783f249459c897a1977516d0e
Jinhan Choi <jinhanchoi1@gmail.com>
2021-03-27 16:17:28 +0900

    [Jinhan] change home api sample

--- core/src/main/kotlin/com/tamerofficial/domain/home/Home.kt
+++ core/src/main/kotlin/com/tamerofficial/domain/home/Home.kt
@@ -3,18 +3,31 @@ package com.tamerofficial.domain.home
 import com.tamerofficial.domain.home.dto.Location
 import com.tamerofficial.domain.home.dto.NearByPlace
 import com.tamerofficial.domain.home.dto.RecommendKnowHow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.reactive.asFlow
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
 
 
 interface Home {
-    suspend fun listNearByPlace(currentLocation : Location) : List<NearByPlace>
+    suspend fun listNearByPlace(currentLocation : Location) : Flow<NearByPlace>
     suspend fun listRecommendKnowHow(loginId : String) : List<RecommendKnowHow>
 }
 
 @Component
 class HomeApp : Home{
-    override suspend fun listNearByPlace(currentLocation : Location) : List<NearByPlace>{
-        return emptyList()
+    override suspend fun listNearByPlace(currentLocation : Location) : Flow<NearByPlace>{
+        return Flux.fromIterable(listOf(
+            NearByPlace(
+                1,
+                "test",
+                "descTest",
+                "https://www.naver.com",
+                Location(1,1),
+                emptyList(),
+                emptyList()
+            )
+        )).asFlow()
     }
 
     override suspend fun listRecommendKnowHow(loginId: String) : List<RecommendKnowHow> {
--- server/src/main/kotlin/com/tamerofficial/app/home/HomeApiController.kt
+++ server/src/main/kotlin/com/tamerofficial/app/home/HomeApiController.kt
@@ -1,25 +1,24 @@
 package com.tamerofficial.app.home
 
 import com.tamerofficial.common.ResponseEntity
+import com.tamerofficial.domain.home.Home
+import com.tamerofficial.domain.home.dto.Location
 import com.tamerofficial.domain.home.dto.NearByPlace
-import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.flow.toList
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Flux
 
 @RequestMapping("/home")
 @RestController
-class HomeApiController {
+class HomeApiController(private val home:Home) {
 
     @GetMapping
     suspend fun listNearByMe() : ResponseEntity<List<NearByPlace>> {
         return ResponseEntity(
             "200",
             "Success",
-            Flux.fromIterable(emptyList<NearByPlace>())
-                .collectList()
-                .awaitFirst()
+            home.listNearByPlace(Location(1,2)).toList()
         )
     }
 }
